b8d27fae35512d5da2f68590851711cc8e9cb605
luisyoroslav <luis@blackfishweb.com>
2021-08-03 04:17:59 -0300

    .

---
+++ 13.0-test/run.ps1
@@ -0,0 +1,7 @@
+param(        
+    [Parameter(Mandatory=$true)]
+    [String]$BlackAddonsSource
+)
+# docker run -u 0  -p 8888:3001 -p 8879:8069 --name odoo-dev  -v C:/SourceCode/git/black-addons:/mnt/black-addons --link db:db -t odoo-dev /usr/bin/python3 -m debugpy --listen 0.0.0.0:3001 /usr/bin/odoo --db_user=odoo --db_host=db --db_password=odoo
+echo "-v ${BlackAddonsSource}:/mnt/black-addons"
+docker run -u 0 -p 8069:8069 -p 8888:8888 --name odoo-dev  -v ${BlackAddonsSource}:/mnt/black-addons --link db:db -t odoo-dev /usr/bin/python3 -m debugpy --listen 0.0.0.0:8888 /usr/bin/odoo --db_user=odoo --db_host=db --db_password=odoo
